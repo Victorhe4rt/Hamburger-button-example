642c561424d228d9c070b054cfffb38e0f3d33b3
Victor Carvalho <62306118+Victorhe4rt@users.noreply.github.com>
2021-04-20 17:11:38 -0300

    Add files via upload

---
+++ Hamburg_button/assets/js/script.js
@@ -0,0 +1,8 @@
+function nav_style(){
+
+ document.getElementById('header').className='header-after'
+ document.getElementById('hamburger').className='btn-format-after'
+ document.getElementById('img-logo-after').className='img-logo-after'
+ document.getElementById('nav-links-after').className='nav-links-after'
+ document.getElementById('nav-after').className="nav-after"
+}
